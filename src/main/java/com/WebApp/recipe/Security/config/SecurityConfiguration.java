ec44bda963bd3a477846e5903d99e67d1cbc7e30
atsiarnouskaya <atsiarnouskaya@mat.umk.pl>
2025-11-29 16:26:23 +0100

    Added OPTIONS method to cross origin configs

--- src/main/java/com/WebApp/recipe/Security/config/SecurityConfiguration.java
+++ src/main/java/com/WebApp/recipe/Security/config/SecurityConfiguration.java
@@ -70,7 +70,7 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://*.up.railway.app"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
